620f8deda004fda1884a646593fbb145eaf86244
Ankit Saxsena <ankit.saxsena@abc.com>
2023-06-30 17:54:38 +0530

    dynamic item wise message changes

--- src/app/bidding-dashboard/bidding-dashboard.component.ts
+++ src/app/bidding-dashboard/bidding-dashboard.component.ts
@@ -6,6 +6,7 @@ import * as Stomp from 'stompjs';
 import * as SockJS from 'sockjs-client';
 import { AuctionItemDetail } from '../auction-item-detail';
 import { LiveBidDetail } from '../live-bid-detail';
+import * as $ from "jquery";
 
 
 @Component({
@@ -23,6 +24,7 @@ export class BiddingDashboardComponent {
   dynMsg:string=''
   isButtonShow:boolean = true
   dynClass='alert alert-success'
+  dynMessages = new Map()
 
   private socket_url='http://192.168.100.155:8080/socket'
   private stompClient:any;
@@ -32,6 +34,7 @@ export class BiddingDashboardComponent {
   constructor(private auctionService: AuctionServiceService,private router: Router,private route:ActivatedRoute) { 
     this.userLoginId = window.localStorage.getItem('userLoginId');
     this.initializeWebSocketConnection()
+    
   }
 
   initializeWebSocketConnection(){
@@ -62,7 +65,11 @@ export class BiddingDashboardComponent {
           this.auctionService.getLiveBidDetails(this.auctionDetailId,this.userLoginId).subscribe(data => {
             console.log(data)
             this.liveBidDetails = data;
+            for(var bidDtl of this.liveBidDetails){
+              this.setDynMessageAfterNewMessages(bidDtl.auctionItemDetail.auctionItemDetailId)
+            }
           });
+          
          }
       });
     });
@@ -84,6 +91,10 @@ export class BiddingDashboardComponent {
       this.liveBidDetails = data;
     });
     console.log(this.auctionDetailId)
+    // $('#item_'+799).show()
+    //     $('#item_'+799).text('Max Bid Changed Successfully')
+    //     $('#item_'+799).addClass('alert-success')
+		// this.setTimerDynDiv('item_799')
   }
 
   ngOnDestroy(): void{
@@ -100,6 +111,7 @@ export class BiddingDashboardComponent {
       //alert('test')
       $('.mat-mdc-form-field-subscript-wrapper').remove();
     });
+  
 }
 
   public saveLiveBidDetail(){
@@ -109,67 +121,48 @@ export class BiddingDashboardComponent {
   public changeMaxBid(auctionItemDetailId:number){
     console.log((<HTMLInputElement>document.getElementById("item_"+auctionItemDetailId)).value);
     let maxBid = parseInt((<HTMLInputElement>document.getElementById("item_"+auctionItemDetailId)).value);
+    if(maxBid != undefined && maxBid != 0 && !isNaN(maxBid)){
     this.auctionService.changeMaxBid(auctionItemDetailId,this.userLoginId,maxBid).subscribe(data => {
       console.log(data)
       if(data.responseCode == 200 && data.message=='Max Bid changed Successfully'){
-        this.dynMsg=data.message
-        this.divHide=false
-          this.setTimer()
-        this.auctionService.getLiveBidDetails(this.auctionDetailId,this.userLoginId).subscribe(data => {
-          console.log(data)
-          this.liveBidDetails = data;
-        });
+        console.log('change bid success executed . . .')
+        this.showDynMessage(auctionItemDetailId,'alert-success',data.message)
+        //this.dynMessages.set(auctionItemDetailId,'alert-success'+'_'+data.message)
     }
     else if(data.responseCode == 200 && data.message=='Item is already alloted to you'){
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     else if(data.responseCode == 200 && data.message=='Item is already sold'){
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     else{
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     });
+  }
+  else{
+    this.showDynMessage(auctionItemDetailId,'alert-danger','Invalid Bid')
+  }
   }
 
   public exitFromItem(auctionItemDetailId:number){
     this.auctionService.exitFromItem(auctionItemDetailId,this.userLoginId).subscribe(data => {
       console.log(data)
       if(data.responseCode == 200 && data.message == 'Exited from item Successfully'){
-        this.dynMsg='Exited from Item Successfully'
-        this.divHide=false
-          this.setTimer()
+        this.showDynMessage(auctionItemDetailId,'alert-success',data.message)
         this.auctionService.getLiveBidDetails(this.auctionDetailId,this.userLoginId).subscribe(data => {
           console.log(data)
           this.liveBidDetails = data;
         });
-    }
+      }
     else if(data.responseCode == 200 && data.message=='Item is already alloted to you'){
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     else if(data.responseCode == 200 && data.message=='Item is already sold'){
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     else{
-      this.dynMsg=data.message
-      this.dynClass='alert alert-danger'
-      this.divHide=false
-        this.setTimer()
+      this.showDynMessage(auctionItemDetailId,'alert-danger',data.message)
     }
     });
   }
@@ -181,4 +174,43 @@ export class BiddingDashboardComponent {
        }, 5000);
      
     }
+
+    public setTimerDynDiv(dynDiv:string){
+      console.log('dyn div set called!')
+      setTimeout(()=>{
+        $('#'+dynDiv).hide();
+        }, 5000);
+      }
+
+      // public dynMessage(auctionItemDetailId:number,cssClass:string,dynMsg:string){
+      //   $('#item_'+auctionItemDetailId).removeClass('alert-success')
+      //   $('#item_'+auctionItemDetailId).removeClass('alert-danger')
+      //   $('#item_'+auctionItemDetailId).show()
+      //   $('#item_'+auctionItemDetailId).text('Max Bid Changed Successfully')
+      //   $('#item_'+auctionItemDetailId).addClass('alert-success')
+      //   setTimeout(()=>{
+      //     $('#item_'+auctionItemDetailId).hide();
+      //     }, 5000);
+      // }
+
+      public showDynMessage(auctionItemDetailId:number,cssClass:string,dynMsg:string){
+        //this.dynMessages.set(auctionItemDetailId,cssClass+'_'+dynMsg)
+        $('#item_dynMsg_'+auctionItemDetailId).removeClass('alert-success')
+        $('#item_dynMsg_'+auctionItemDetailId).removeClass('alert-danger')
+        $('#item_dynMsg_'+auctionItemDetailId).show()
+        $('#item_dynMsg_'+auctionItemDetailId).text(dynMsg)
+        $('#item_dynMsg_'+auctionItemDetailId).addClass(cssClass)
+        setTimeout(()=>{
+          $('#item_dynMsg_'+auctionItemDetailId).hide();
+          //this.dynMessages.delete(auctionItemDetailId)
+          }, 5000);
+      }
+
+      public setDynMessageAfterNewMessages(auctionItemDetailId:number){
+        if(this.dynMessages.get(auctionItemDetailId) != undefined){
+          let cssClass = this.dynMessages.get(auctionItemDetailId).toString().split('_')[0]
+          let message = this.dynMessages.get(auctionItemDetailId).toString().split('_')[1]
+        this.showDynMessage(auctionItemDetailId,cssClass,message)
+      }
+      }
 }
